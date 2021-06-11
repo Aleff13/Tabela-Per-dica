e1cade2a11f07bc75ce25b1470c0da48feb21ff4
Aleff13 <aleff.menna.resler@gmail.com>
2021-06-11 17:40:03 -0300

    html-css-js
    ss1

---
+++ tabela/tabela.js
@@ -0,0 +1,293 @@
+var h = window.document.getElementById('h')
+var he = window.document.getElementById("He")
+var li = window.document.getElementById("Li")
+var be = window.document.getElementById("Be")
+var b = window.document.getElementById("B")
+var c = window.document.getElementById("C")
+var n = window.document.getElementById("N")
+var o = window.document.getElementById("O")
+var f = window.document.getElementById("F")
+var ne = window.document.getElementById("Ne")
+var na = window.document.getElementById('Na')
+var mg = window.document.getElementById('Mg')
+var al = window.document.getElementById('Al')
+var si = window.document.getElementById('Si')
+var p = window.document.getElementById('P')
+var s = window.document.getElementById('S')
+var cl = window.document.getElementById('Cl')
+var ar = window.document.getElementById('Ar')
+var k = window.document.getElementById('K')
+var ca = window.document.getElementById('Ca')
+var sc = window.document.getElementById('Sc')
+var ti = window.document.getElementById('Ti')
+var v = window.document.getElementById('V')
+var cr = window.document.getElementById('Cr')
+var mn = window.document.getElementById('Mn')
+var fe = window.document.getElementById('Fe')
+var co = window.document.getElementById('Co')
+var ni = window.document.getElementById('Ni')
+var cu = window.document.getElementById('Cu')
+var zn = window.document.getElementById('Zn')
+var ga = window.document.getElementById('Ga')
+var ge = window.document.getElementById('Ge')
+var as = window.document.getElementById('As')
+var se = window.document.getElementById('Se')
+var br = window.document.getElementById('Br')
+var xr = window.document.getElementById('Xr')
+
+
+function passar() {
+    h.style.width="100px"
+    h.style.height="50px"
+    h.innerText='Hidrogênio'
+
+}
+function saiu(){
+    h.style.width="40px"
+    h.style.height="30px"
+    h.innerText="H"
+}
+function clicou(){
+    window.location.href='hidrogenio.html'
+}
+function passar1() {
+    he.style.width="100px"
+    he.style.height="50px"
+    he.innerText='Hélio'
+
+}
+function saiu1(){
+    he.style.width="40px"
+    he.style.height="30px"
+    he.innerText="He"
+}
+function clicou1(){
+    window.location.href='helio.html'
+}
+function passar2() {
+    li.style.width="100px"
+    li.style.height="50px"
+    li.innerText='Litio'
+
+}
+function saiu2(){
+    li.style.width="40px"
+    li.style.height="30px"
+    li.innerText="Li"
+}
+function clicou2(){
+    window.location.href = 'litio.html'
+}
+function passar3() {
+    be.style.width="100px"
+    be.style.height="50px"
+    be.innerText='Berílio'
+
+}
+function saiu3(){
+    be.style.width="40px"
+    be.style.height="30px"
+    be.innerText="Be"
+
+}
+function clicou3(){
+   window.location.href = 'berilio.html'
+}
+function passar4() {
+    b.style.width="100px"
+    b.style.height="50px"
+    b.innerText='Boro'
+
+}
+function saiu4(){
+    b.style.width="40px"
+    b.style.height="30px"
+    b.innerText="B"
+
+}
+function clicou4(){
+    window.location.href = 'boro.html'
+}
+function passar5() {
+    c.style.width="100px"
+    c.style.height="50px"
+    c.innerText='Carbono'
+
+}
+function saiu5(){
+    c.style.width="40px"
+    c.style.height="30px"
+    c.innerText="C"
+}
+function clicou5(){
+    window.location.href = 'carbono.html'
+}
+function passar6() {
+    n.style.width="100px"
+    n.style.height="50px"
+    n.innerText='Nitrogênio'
+
+}
+function saiu6(){
+    n.style.width="40px"
+    n.style.height="30px"
+    n.innerText="N"
+}
+function clicou6(){
+    window.location.href = 'nitrogenio.html'
+}
+function passar7() {
+    o.style.width="100px"
+    o.style.height="50px"
+    o.innerText='Oxigênio'
+
+}
+function saiu7(){
+    o.style.width="40px"
+    o.style.height="30px"
+    o.innerText="O"
+}
+function clicou7(){
+    window.location.href = 'oxigenio.html'
+}
+function passar8() {
+    f.style.width="100px"
+    f.style.height="50px"
+    f.innerText='Flúor'
+
+}
+function saiu8(){
+    f.style.width="40px"
+    f.style.height="30px"
+    f.innerText="F"
+}
+function clicou8(){
+    window.location.href = 'fluor.html'
+}
+function passar9() {
+    ne.style.width="100px"
+    ne.style.height="50px"
+    ne.innerText='Neônio'
+
+}
+function saiu9(){
+    ne.style.width="40px"
+    ne.style.height="30px"
+    ne.innerText="Ne"
+}
+function clicou9(){
+    window.location.href = 'neonio.html'
+}
+function passar10() {
+    na.style.width="100px"
+    na.style.height="50px"
+    na.innerText='Sódio'
+
+}
+function saiu10(){
+    na.style.width="40px"
+    na.style.height="30px"
+    na.innerText="Na"
+}
+function clicou10(){
+    window.alert('numero atomico = 11, Peso atômico (massa atômica relativa): 22,99')
+}
+function passar11() {
+    mg.style.width="100px"
+    mg.style.height="50px"
+    mg.innerText='Magnésio'
+
+}
+function saiu11(){
+    mg.style.width="40px"
+    mg.style.height="30px"
+    mg.innerText="Mg"
+}
+function clicou11(){
+    window.alert('numero atomico = 12, Peso atômico (massa atômica relativa): 24,305')
+}
+function passar12() {
+    al.style.width="100px"
+    al.style.height="50px"
+    al.innerText='Alumínio'
+
+}
+function saiu12(){
+    al.style.width="40px"
+    al.style.height="30px"
+    al.innerText="Al"
+}
+function clicou12(){
+    window.alert('numero atomico = 13, Peso atômico (massa atômica relativa): 26,982')
+}
+function passar13() {
+    si.style.width="100px"
+    si.style.height="50px"
+    si.innerText='Silício'
+
+}
+function saiu13(){
+    si.style.width="40px"
+    si.style.height="30px"
+    si.innerText="Si"
+}
+function clicou13(){
+    window.alert('numero atomico = 14, Peso atômico (massa atômica relativa): 28,086')
+}
+function passar14() {
+    p.style.width="100px"
+    p.style.height="50px"
+    p.innerText='Fósforo'
+
+}
+function saiu14(){
+    p.style.width="40px"
+    p.style.height="30px"
+    p.innerText="P"
+}
+function clicou14(){
+    window.alert('numero atomico = 15, Peso atômico (massa atômica relativa): 30,974')
+}
+function passar15() {
+    s.style.width="100px"
+    s.style.height="50px"
+    s.innerText='Enxofre'
+
+}
+function saiu15(){
+    s.style.width="40px"
+    s.style.height="30px"
+    s.innerText="S"
+}
+function clicou15(){
+    window.alert('numero atomico = 16, Peso atômico (massa atômica relativa): 32,095')
+}
+function passar16() {
+    cl.style.width="100px"
+    cl.style.height="50px"
+    cl.innerText='Cloro'
+
+}
+function saiu16(){
+    cl.style.width="40px"
+    cl.style.height="30px"
+    cl.innerText="Cl"
+}
+function clicou16(){
+    window.alert('numero atomico = 17, Peso atômico (massa atômica relativa): 35,453')
+}
+function passar17() {
+    ar.style.width="100px"
+    ar.style.height="50px"
+    ar.innerText='Argônio'
+
+}
+function saiu17(){
+    ar.style.width="40px"
+    ar.style.height="30px"
+    ar.innerText="Ar"
+}
+function clicou17(){
+    document.write('numero atomico = 18</br> Peso atômico (massa atômica relativa): 39,948')
+}
+
